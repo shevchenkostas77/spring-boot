7ed8af9d7457fbac33df2211324a343f64f482d8
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-10-14 08:16:08 +0300

    Building Spring Boot Applications

---
+++ src/main/java/com/shevchenkostas77/spring/springboot/spring_course_springboot/SpringCourseSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.shevchenkostas77.spring.springboot.spring_course_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseSpringbootApplication.class, args);
+    }
+}
