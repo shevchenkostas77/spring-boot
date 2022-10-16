c0edcbf5befa0b48d1c3149873d67f59fce4227b
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-10-16 18:41:27 +0300

    Building a REST API. Part 1

---
+++ src/main/java/com/shevchenkostas77/spring/springboot/spring_course_springboot/dao/EmployeeDAOImpl.java
@@ -0,0 +1,55 @@
+package com.shevchenkostas77.spring.springboot.spring_course_springboot.dao;
+
+import com.shevchenkostas77.spring.springboot.spring_course_springboot.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        Session session = entityManager.unwrap(Session.class);
+
+        Query<Employee> query = session.createQuery("from Employee"
+                , Employee.class);
+        List<Employee> allEmplyees = query.getResultList();
+
+        return allEmplyees;
+    }
+
+//    @Override
+//    public void saveEmployee(Employee employee) {
+//        Session session = sessionFactory.getCurrentSession();
+//        session.saveOrUpdate(employee);
+//    }
+//
+//    @Override
+//    public Employee getEmployee(int id) {
+//        Session session = sessionFactory.getCurrentSession();
+//        Employee employee = session.get(Employee.class, id);
+//        return employee;
+//    }
+//
+//    @Override
+//    public void deleteEmployee(int id) {
+//        Session session = sessionFactory.getCurrentSession();
+//
+////        Employee employee = session.get(Employee.class, id);
+////        session.delete(employee);
+//
+//        Query<Employee> query = session.createQuery("delete from Employee " +
+//                "where id = :employeeId");
+//        query.setParameter("employeeId", id);
+//        query.executeUpdate();
+//    }
+
+}
---
+++ src/main/java/com/shevchenkostas77/spring/springboot/spring_course_springboot/service/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.shevchenkostas77.spring.springboot.spring_course_springboot.service;
+
+import com.shevchenkostas77.spring.springboot.spring_course_springboot.dao.EmployeeDAO;
+import com.shevchenkostas77.spring.springboot.spring_course_springboot.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional // спринг автоматически управляет открытием и закрытием транзакций
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+//    @Override
+//    @Transactional
+//    public void saveEmployee(Employee employee) {
+//        employeeDAO.saveEmployee(employee);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public Employee getEmployee(int id) {
+//        return employeeDAO.getEmployee(id);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void deleteEmployee(int id) {
+//        employeeDAO.deleteEmployee(id);
+//    }
+}
